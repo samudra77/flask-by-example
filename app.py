678eb01c3c28ddc2d78a62c7f369465eca0c6743
Sumana Sarma <Sumana@Sumanas-MacBook-Pro.local>
2017-03-04 22:36:49 -0500

    config

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import os
 from flask import Flask
-app = Flask(__name__)
 
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+print(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
@@ -13,3 +17,4 @@ def hello_name(name):
 
 if __name__ == '__main__':
     app.run()
+    
